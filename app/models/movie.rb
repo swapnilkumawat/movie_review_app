19fcd1bb916dfe9a129e454756ff005ec005da47
Swapnil Kumawat <swapnil.kumawat@zingoy.com>
2023-08-15 19:25:31 +0530

    Association added for reviews in movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
 
+  has_many :reviews
+
   scope :sorted_by_average_stars, -> {
     joins("LEFT JOIN reviews ON movies.id = reviews.movie_id")
       .group("movies.id")
