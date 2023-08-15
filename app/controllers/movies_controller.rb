443dd610d3200cf581a4a81e08cf5829deff0bae
Swapnil Kumawat <swapnil.kumawat@zingoy.com>
2023-08-15 18:17:23 +0530

    Create Actor Search Form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,9 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    if params[:actor_search].present?
+      @movies = @movies.where("actors @> ARRAY[?]::varchar[]", params[:actor_search])
+    end
   end
 
 end
